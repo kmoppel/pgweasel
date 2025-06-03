efce9d4bc80454f17e034a10d142e6221263fa76
Kaarel Moppel <kaarel.moppel@gmail.com>
2025-06-03 12:23:37 +0300

    Add cmd/peaks.go

---
+++ cmd/peaks.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"time"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+)
+
+var PeakBucketIntervalStr string = "10m" // Default bucket interval as a string
+var PeakBucketDuration time.Duration
+
+var peaksCmd = &cobra.Command{
+	Use:   "peaks [--bucket $BUCKET_INTERVAL] [$LOG_FILE_OR_FOLDER]",
+	Short: "Identify periods where most log entries are emitted, per severity level",
+	Run: func(cmd *cobra.Command, args []string) {
+		Peaks = true
+		var err error
+		PeakBucketDuration, err = time.ParseDuration(PeakBucketIntervalStr)
+		if err != nil {
+			log.Fatal().Err(err).Msgf("Invalid --bucket input: %s", PeakBucketIntervalStr)
+		}
+		showErrors(cmd, args)
+	},
+	Aliases: []string{"peak", "busy"},
+}
+
+func init() {
+	rootCmd.AddCommand(peaksCmd)
+
+	peaksCmd.Flags().StringVarP(&PeakBucketIntervalStr, "bucket", "b", "10m", "Bucket interval")
+}
