c49ecc0d5d71a3d3276a89df8a86856731cd53c4
Gints Polis <polis.gints@gmail.com>
2025-12-15 18:31:13 +0200

    Bit stricter errors

--- src/error.rs
+++ src/error.rs
@@ -1,2 +1,49 @@
+use derive_more::From;
+
 pub type Result<T> = core::result::Result<T, Error>;
-pub type Error = Box<dyn std::error::Error>;
+
+#[derive(Debug, From)]
+pub enum Error {
+    // Enable this if you want custom errors (also see region below)
+    #[from]
+    Custom(String),
+
+    // -- Parsers
+
+
+    // -- Externals
+    #[from]
+    Io(std::io::Error),
+
+    #[from]
+    Zip(zip::result::ZipError)
+
+}
+
+// region:    --- Custom --- Uncomment if want custom errors
+
+impl Error {
+    pub fn custom(val: impl std::fmt::Display) -> Self {
+        Self::Custom(val.to_string())
+    }
+}
+
+impl From<&str> for Error {
+    fn from(val: &str) -> Self {
+        Self::Custom(val.to_string())
+    }
+}
+
+// endregion: --- Custom
+
+// region:    --- Error Boilerplate
+
+impl core::fmt::Display for Error {
+    fn fmt(&self, fmt: &mut core::fmt::Formatter) -> core::result::Result<(), core::fmt::Error> {
+        write!(fmt, "{self:?}")
+    }
+}
+
+impl std::error::Error for Error {}
+
+// endregion: --- Error Boilerplate
