52ed6e06f043b36066af473e9a903fd92697cf04
Gints Polis <polis.gints@gmail.com>
2025-12-17 19:55:32 +0200

    full result as debug message

--- src/errors/process.rs
+++ src/errors/process.rs
@@ -29,6 +29,7 @@ pub fn process_errors(converted_args: ConvertedArgs, min_severity: &Severity) ->
             converted_args.end,
         ) {
             let result = record?;
+            debug!("Log line received: {result:?}");
             println!("{}", result.raw);
         }
         debug!("Finished in: {:?}", start.elapsed());
